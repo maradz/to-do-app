f6bed3ecb98f9525497f2814f9b8abec7fe97a02
Marcelina Radziejewska <marcelinaaradziejewska@gmail.com>
2020-08-02 18:29:02 +0200

    Updates

---
+++ precache-manifest.cf9231e59df6297fc57da6d22460304a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c44fd56deecfef95791835dfe200b2",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "6a8894b232136a2868c5",
+    "url": "/to-do-app/static/css/main.da226ef9.chunk.css"
+  },
+  {
+    "revision": "b6dc45a21e9f11088119",
+    "url": "/to-do-app/static/js/2.6e8ac1d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.6e8ac1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8894b232136a2868c5",
+    "url": "/to-do-app/static/js/main.e75b69bd.chunk.js"
+  },
+  {
+    "revision": "7662e794f6188420f77f",
+    "url": "/to-do-app/static/js/runtime-main.3d9f550e.js"
+  }
+]);
